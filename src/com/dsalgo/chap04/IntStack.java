0ca42c6c112613758f6e73c11b0fecd04daeb2a5
ej2kd <eiji.tsukada9415@gmail.com>
2021-10-31 18:36:20 +0900

    add chap04:Stack

---
+++ src/com/dsalgo/chap04/IntStack.java
@@ -0,0 +1,93 @@
+package com.dsalgo.chap04;
+
+public class IntStack {
+    private int[] stk; // 格納する配列
+    private int capacity; // スタックの容量
+    private int ptr; // スタックポインタ
+
+    //    実行時例外：スタックが空
+    public class EmptyIntStackException extends RuntimeException {
+        public EmptyIntStackException() { }
+    }
+
+    //    実行時例外：スタックが満杯
+    public class OverflowIntStackException extends RuntimeException {
+        public OverflowIntStackException() { }
+    }
+
+    public IntStack(int maxlen) {
+        ptr = 0;
+        capacity = maxlen;
+        try {
+            stk = new int[capacity]; // スタック本体用の配列
+        } catch (OutOfMemoryError e) { // スタックの生成に失敗
+            capacity = 0;
+        }
+    }
+
+//    スタックにxをプッシュする
+    public int push(int x) throws OverflowIntStackException {
+        if (ptr >= capacity) { // スタックが満杯である時
+            throw new OverflowIntStackException();
+        }
+        return stk[ptr++] = x; // 先にスタックにデータを格納してから、ポインタ値をインクリメントする
+    }
+
+//    スタックからデータをポップする
+    public int pop() throws EmptyIntStackException {
+        if (ptr <= 0) { // スタックが空である時
+            throw new EmptyIntStackException();
+        }
+        return stk[--ptr]; // 先にポインタ値をデクリメントしてから、スタックのデータを取り出す
+    }
+
+//    スタックからデータをピークする（参照するだけ）
+    public int peek() throws EmptyIntStackException {
+        if (ptr <= 0) { // スタックが空である時
+            throw new EmptyIntStackException();
+        }
+        return stk[ptr - 1];
+    }
+
+//    スタックを空にする
+    public void clear() {
+        ptr = 0;
+    }
+
+//    スタックからxを探す
+    public int indexOf(int x) {
+        for (int i = ptr - 1; i >= 0; i--) { // From top to bottom
+            if (stk[i] == x) {
+                return i; // Success
+            }
+        }
+        return -1; // Fail
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int size() {
+        return ptr;
+    }
+
+    public boolean isEmpty() {
+        return ptr <= 0;
+    }
+
+    public boolean isFull() {
+        return ptr >= capacity;
+    }
+
+    public void dump() {
+        if (ptr <= 0) {
+            System.out.println("スタックは空です。");
+        } else {
+            for (int i = 0; i < ptr; i++) {
+                System.out.print(stk[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
